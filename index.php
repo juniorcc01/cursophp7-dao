65e9c3179c5924e125327a35584679333a505704
c.oliveira <juniorcc01@yahoo.com.br>
2018-03-19 15:11:30 -0300

    implemento da classe usuário

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
 
 require_once("config.php");
+//Carrega 1 usuario;
+//$root = new Usuario();
+//$root->loadbyId(1);
+//echo $root;
 
-$root = new Usuario();
+// Carrega uma lista de usuarios
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
 
-$root->loadbyId(1);
+//Carrega lista de usuarios procurando pelo login
+/*
+$search = Usuario::search("ju");
+echo json_encode($search);
+*/
 
-echo $root;
+//Carrega um usuario usando login e senha.
 
+$usuario = new Usuario();
+
+$usuario->login("user","123456");
+
+echo $usuario;
 ?>
